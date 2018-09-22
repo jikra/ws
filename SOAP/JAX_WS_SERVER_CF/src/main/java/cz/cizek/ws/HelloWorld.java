30979058b77472a863b0ef0a3a98b22bb2970943
Jiří Čížek <cizek.jy@gmail.com>
2018-09-22 10:17:44 +0200

    solve jax ws server CF

--- SOAP/JAX_WS_SERVER_CF/src/main/java/cz/cizek/ws/HelloWorld.java
+++ SOAP/JAX_WS_SERVER_CF/src/main/java/cz/cizek/ws/HelloWorld.java
@@ -1,5 +1,17 @@
 package cz.cizek.ws;
 
-public class HelloWorld {
+import javax.jws.WebService;
 
+@WebService(endpointInterface = "cz.cizek.ws.SayHelloPT")
+public class HelloWorld implements SayHelloPT {
+
+    @Override
+    public SayHelloResponse sayHello(SayHelloRequest parameters) {
+
+        SayHelloResponse sayHelloResponse = new SayHelloResponse();
+
+        sayHelloResponse.setGreeting("AHOJ");
+
+        return sayHelloResponse;
+    }
 }
